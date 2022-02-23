11f55f30b90aec34615abff2f3d70686c8ccf8f1
Alexander L. Perez <alexander.l.perez@outlook.com>
2022-02-23 14:50:11 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
-sudo apt install libatlas-base-dev -y
+sudo apt install libatlas-base-dev python3-pil python3-pil.imagetk -y
 pip3 install pandas exifread GPSPhoto piexif pyyaml Pillow pygpsclient
 pip3 install --upgrade numpy
+pip3 install --upgrade pygpsclient
