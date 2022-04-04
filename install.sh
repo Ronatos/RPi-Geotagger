3e0fc1010123d57c731e62b94be8736beed538ea
Alexander L. Perez <alexander.l.perez@outlook.com>
2022-04-04 09:24:30 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,6 @@ sudo apt install libatlas-base-dev python3-pil python3-pil.imagetk -y
 pip3 install pandas exifread GPSPhoto piexif pyyaml Pillow pygpsclient
 pip3 install --upgrade numpy
 pip3 install --upgrade pygpsclient
+cp aerial-survey.yaml ~/
+cp auto_capture.py ~/
+cp GPS_Post_Processing.py ~/
